bb464753f42a362e47e4ed3529890b873a61ef96
BasseND <ndiayebasse@gmail.com>
2019-07-27 20:37:06 +0200

    Init commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// var vacationSpots = ['Paris', 'New York', 'Barcelona'];
+
+// for (var i = vacationSpots.length - 1; i >= 2; i--) {
+//   console.log('I would love to visit ' + vacationSpots[i]);
+// }
+// VM345: 4 I would love to visit Barcelona
+
+
+$(document).ready(function () {
+  // Variables
+  var items = document.querySelectorAll('.need-help-thumbnail');
+  var loadMoreBtn = document.querySelector('.load_more_btn');
+  var spinner = document.createElement("div");
+
+  // Hide all items except the first 3 items
+  for (var i = items.length - 1; 3 <= i; i--) {
+    items[i].classList = 'need-help-thumbnail hidden-item ';
+  }
+
+  // Loading all items
+  loadItems();
+  function loadItems() {
+    loadMoreBtn.addEventListener('click', function (e) {
+      e.preventDefault();
+      buildSpinner();
+      setTimeout(function () {
+        removeSpinner();
+        for (var i = 0; i < items.length; i++) {
+          items[i].classList = 'need-help-thumbnail grow-anim';
+        }
+      }, 2000);
+    })
+  }
+  // Build Spinner 
+  function buildSpinner() {
+    var loadMoreTitle = document.querySelector('.load-more-title');
+    spinner.classList = 'spinner lds-css';
+    spinner.innerHTML = '<div class="lds-spin" style="width:100%;height:100%"><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>';
+    loadMoreBtn.insertBefore(spinner, loadMoreTitle);
+  }
+  // Removing the spinner
+  function removeSpinner() {
+    spinner.remove();
+  }
+});
+
